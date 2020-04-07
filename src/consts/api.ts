ae678f7d90c21767033fa69fe14a035546781dc6
k.ivanov <v.b.ingram@gmail.com>
2020-04-07 18:49:56 +0300

    DEV-5 remove axios, update api for back4app

---
+++ src/consts/api.ts
@@ -0,0 +1,9 @@
+export const API_SERVER = 'https://parseapi.back4app.com';
+export const API_HEADERS_APP_ID = 'EQcvbyNyoLzau2AWdxwjUmtYj2JoYBblqgL8cOIO';
+export const API_HEADERS_REST_API = 'k5w0RzUWBwg618JIrQuSSzALViLjiIhcGnHUc8Xs';
+
+export const API_HEADERS = {
+  'X-Parse-Application-Id': API_HEADERS_APP_ID,
+  'X-Parse-REST-API-Key': API_HEADERS_REST_API,
+  'Content-Type': 'application/json'
+}
